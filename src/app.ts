5e7d9b64fa2af472eafb5cb9ded2d3c921978e73
agrant216 <agrant12@kent.edu>
2019-08-28 18:51:16 +0000

    game works-ish

---
+++ src/app.ts
@@ -0,0 +1,34 @@
+import { getRandomInt } from './random';
+
+export function runApp() {
+    const secretNumber = getRandomInt(1, 6);
+    const squares = document.querySelectorAll('.square');
+    let currentSquare = 1;
+    squares.forEach((s: HTMLDivElement, k) => {
+        if (currentSquare === secretNumber) {
+            s.dataset.secret = 'true';
+        }
+        s.addEventListener('click', handleClick);
+        currentSquare++;
+    });
+}
+function handleClick() {
+    const clickedSquare = this as HTMLDivElement;
+    const isSpecial = clickedSquare.dataset.secret === 'true';
+    if (isSpecial) {
+        clickedSquare.classList.add('winner');
+        setLosers(document.querySelectorAll('.square'));
+    } else {
+        clickedSquare.classList.add('loser');
+        clickedSquare.removeEventListener('click', handleClick);
+    }
+    console.log('You clicked on ', this);
+}
+function setLosers(squares: NodeList) {
+    squares.forEach((s: HTMLDivElement) => {
+        if (s.dataset.secret !== 'true') {
+            s.classList.add('loser');
+            s.removeEventListener('click', handleClick);
+        }
+    });
+}
